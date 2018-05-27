1018833c089f049cc4db7c692ba0796cd1b8b54c
Nate Turner <nater1992@gmail.com>
2018-05-27 10:33:26 -0700

    logging times now

--- build.sbt
+++ build.sbt
@@ -8,3 +8,4 @@ libraryDependencies += "com.typesafe.akka" %% "akka-http"   % "10.1.1"
 libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.1"
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.11" // or whatever the latest version is
 libraryDependencies += "io.spray" %%  "spray-json" % "1.3.3"
+libraryDependencies += "joda-time" % "joda-time" % "2.9.9"
--- src/main/scala/SalesPipelineEventWebServer.scala
+++ src/main/scala/SalesPipelineEventWebServer.scala
@@ -1,53 +1,55 @@
-import java.time.LocalTime
-
-import akka.{Done, NotUsed}
-import akka.actor.{Actor, ActorRef, ActorSystem, Cancellable, Props, Terminated}
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.sse.ServerSentEvent
-import akka.http.scaladsl.server.Directives._
-import akka.stream.{ActorMaterializer, ClosedShape}
-
-import scala.io.StdIn
-import akka.stream.scaladsl.{Broadcast, BroadcastHub, Flow, GraphDSL, Keep, RunnableGraph, Sink, Source}
-import java.time.format.DateTimeFormatter.ISO_LOCAL_TIME
-
 import HelloScalaStreams.Worker
+import akka.NotUsed
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Terminated}
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.Marshal
+import org.joda.time.{DateTime => JodaDT}
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.sse.ServerSentEvent
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.routing.{BroadcastRoutingLogic, Router}
-import akka.stream.actor.ActorPublisher
-import org.reactivestreams.Publisher
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{BroadcastHub, Keep, Source}
+import org.joda.time.format.DateTimeFormat
+import spray.json.{DefaultJsonProtocol, RootJsonFormat, _}
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.concurrent.duration._
-import spray.json._
-
-import scala.util.{Failure, Success}
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.io.StdIn
 
 final case class SalesPipelineEvent(
   event_name: String,
   productId: Int,
   prospectId: String,
-  additionalInfo: Map[String, String]
+  additionalInfo: Map[String, String],
+  reportedTime: Option[JodaDT],
+  applicationTime: Option[JodaDT],
 )
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val salesPipelineEventFormat: RootJsonFormat[SalesPipelineEvent] = jsonFormat4(SalesPipelineEvent)
-}
+  private val elasticSearchDateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+  implicit object JodaJsonFormat extends RootJsonFormat[JodaDT] {
+    def write(c: JodaDT) =
+      JsString(elasticSearchDateTimeFormat.print(c))
 
+    def read(value: JsValue): JodaDT = value match {
+      case JsString(dateString) =>
+        elasticSearchDateTimeFormat.parseDateTime(dateString)
+      case _ => deserializationError("Color expected")
+    }
+  }
+  implicit val salesPipelineEventFormat: RootJsonFormat[SalesPipelineEvent] = jsonFormat6(SalesPipelineEvent)
+}
 
 object SalesPipelineEventWebServer extends JsonSupport {
 
   implicit val system: ActorSystem = ActorSystem("my-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  // needed for the future flatMap/onComplete in the end
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-
 
   def logToElasticsearch(event: SalesPipelineEvent): Future[SalesPipelineEvent] = {
     val responseFuture = Marshal(event).to[RequestEntity] flatMap { entity =>
@@ -79,141 +81,99 @@ object SalesPipelineEventWebServer extends JsonSupport {
       }
     }
   }
+
   class ElasticsearchLoggingActor extends Actor {
     override def receive: Receive = {
-      case event:SalesPipelineEvent =>
+      case event: SalesPipelineEvent =>
         logToElasticsearch(event)
     }
   }
+
   class EventsActor extends Actor {
-    val loggingActor = context.actorOf(Props[ElasticsearchLoggingActor])
-    var router: Router = {
+    // Logs all received events to elasticsearch
+    private val loggingActor = context.actorOf(Props[ElasticsearchLoggingActor])
+
+    // Routes to server sent events actors for real-time display in client applications
+    var sseRouter: Router = {
       Router(BroadcastRoutingLogic(), Vector.empty)
     }
-    var items:List[SalesPipelineEvent] = List.empty
+
+    // A list of sales pipeline events this has received
+    var items: List[SalesPipelineEvent] = List.empty
 
     def receive: PartialFunction[Any, Unit] = {
-      case event:SalesPipelineEvent =>
+      case event: SalesPipelineEvent =>
         println("got " + event.toString)
         items = items :+ event
         loggingActor ! event
-        router.route(event, sender())
+        sseRouter.route(event, sender())
       case x: ActorRef ⇒
         println("adding routee")
-        router = router.addRoutee(x)
+        sseRouter = sseRouter.addRoutee(x)
       case Terminated(a) ⇒
         println("terminated")
-        router = router.removeRoutee(a)
+        sseRouter = sseRouter.removeRoutee(a)
         val r = context.actorOf(Props[Worker])
         context watch r
-        router = router.addRoutee(r)
+        sseRouter = sseRouter.addRoutee(r)
     }
   }
 
-  val actorRef: ActorRef = system.actorOf(Props[ActorBasedSource])
-  val pub: Publisher[SalesPipelineEvent] = ActorPublisher[SalesPipelineEvent](actorRef)
-
-  val eventsActor = system.actorOf(Props[EventsActor])
-
-//  val eventsSource: Source[SalesPipelineEvent, NotUsed] = Source.fromPublisher(pub)
-//  Source.fromPublisher(pub)
-//    .runForeach(e => {
-//      println(e.event_name  )
-//    })
+  val eventsActor: ActorRef = system.actorOf(Props[EventsActor])
 
   def eventsRoute: Route = {
     import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
 
-    implicit val salesPipelineEventFormat: RootJsonFormat[SalesPipelineEvent] = jsonFormat4(SalesPipelineEvent)
+    implicit val salesPipelineEventFormat: RootJsonFormat[SalesPipelineEvent] = jsonFormat6(SalesPipelineEvent)
 
     path("events") {
       get {
         respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*")) {
-            val (sseActor: ActorRef, sseSource: Source[ServerSentEvent, NotUsed]) =
-              Source.actorRef[SalesPipelineEvent](10, akka.stream.OverflowStrategy.dropTail)
-                .map(s => {
-                  println("new event: ", s.toJson.toString)
-                  ServerSentEvent(s.toJson.toString())
-                })
-                .keepAlive(1.second, () => ServerSentEvent.heartbeat)
-                .recover {
-                  case t: RuntimeException ⇒ {
-                    println(t.getMessage)
-                    ServerSentEvent(JsObject(
-                      "err" -> JsString(t.getMessage)
-                    ).toString())
-                  }
+          val (sseActor: ActorRef, sseSource: Source[ServerSentEvent, NotUsed]) =
+            Source.actorRef[SalesPipelineEvent](10, akka.stream.OverflowStrategy.dropTail)
+              .map(s => {
+                println("new event: ", s.toJson.toString)
+                ServerSentEvent(s.toJson.toString())
+              })
+              .keepAlive(1.second, () => ServerSentEvent.heartbeat)
+              .recover {
+                case t: RuntimeException ⇒ {
+                  println(t.getMessage)
+                  ServerSentEvent(JsObject(
+                    "err" -> JsString(t.getMessage)
+                  ).toString())
                 }
-                .toMat(BroadcastHub.sink[ServerSentEvent])(Keep.both)
-                .run()
+              }
+              .toMat(BroadcastHub.sink[ServerSentEvent])(Keep.both)
+              .run()
 
-              eventsActor ! sseActor
+          eventsActor ! sseActor
 
           complete(sseSource)
         }
       } ~
-      post {
-        entity(as[SalesPipelineEvent]) { event =>
-          respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*")) {
-            complete {
-              eventsActor ! event
-
-              "success`!"
+        post {
+          entity(as[SalesPipelineEvent]) { event =>
+            eventsActor ! event.copy(applicationTime = Some(JodaDT.now))
+            respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*")) {
+              complete("")
             }
           }
         }
-      }
     }
   }
 
   def main(args: Array[String]) {
-
-
-//    val route =
-//      path("hello") {
-//        get {
-//          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-//        }
-//      }
-
     val route = eventsRoute
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8081)
 
-
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+
     StdIn.readLine() // let it run until user presses return
+
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
-  }
-}
-
-class ActorBasedSource extends Actor with ActorPublisher[SalesPipelineEvent]{
-  import akka.stream.actor.ActorPublisherMessage._
-  var items:List[SalesPipelineEvent] = List.empty
-
-  def receive: PartialFunction[Any, Unit] = {
-    case s:SalesPipelineEvent =>
-      println("got " + s.toString)
-      if (totalDemand == 0)
-        items = items :+ s
-      else
-        onNext(s)
-
-    case Request(demand) =>
-      if (demand > items.size){
-        items foreach (onNext)
-        items = List.empty
-      }
-      else{
-        val (send, keep) = items.splitAt(demand.toInt)
-        items = keep
-        send foreach (onNext)
-      }
-
-
-    case other =>
-      println(s"got other $other")
   }
 }
